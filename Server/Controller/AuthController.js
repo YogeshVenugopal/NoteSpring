d3dcd0edc0335d47ef8eabfc533ea945ae1546a6
YogeshVenugopal <yogeshvenugopal875@gmail.com>
2026-07-10 10:17:47 +0530

    feat: added the feature of sending verification OTP

--- Server/Controller/AuthController.js
+++ Server/Controller/AuthController.js
@@ -3,6 +3,7 @@ import user from "../Model/Usermodel.js";
 import { validEmail } from "../Util/checkValidEmail.js";
 import hashPassword from "../Util/hashPassword.js";
 import { signToken } from "../Util/signToken.js";
+import transporter from "../Config/mailer.js";
 
 export const register = async (req, res) => {
     const { username, email, password } = req.body;
@@ -37,6 +38,15 @@ export const register = async (req, res) => {
             sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
             maxAge: 7 * 24 * 60 * 60 * 1000
         })
+
+        const mailOptions = {
+            from: process.env.SENDER_EMAIL,
+            to: email,
+            subject: "Welcome to NoteSpring",
+            text: `Hello ${username},\n\nWelcome to NoteSpring! We're excited to have you on board. Start exploring and enjoy the features we offer.\n\nBest regards,\nThe NoteSpring Team`
+        }
+
+        await transporter.sendMail(mailOptions);
 
         return res.json({
             success: true,
@@ -124,6 +134,56 @@ export const logout = async(req, res) => {
         return res.json({
             success: false,
             message: error
+        })
+    }
+}
+
+export const sendVerifyOtp = async(req, res) => {
+    try {
+
+        const { userId } = req.body;
+
+        const isUser = await user.findById(userId);
+
+        if(!isUser){
+            return res.status(404).json({
+                success: false,
+                message: "User not found"
+            })
+        }
+
+        if(isUser.isAccountVerified){
+            return res.json({
+                success: false,
+                message: "Account already verified"
+            })
+        }
+
+        const otp = String(Math.floor(100000 + Math.random() * 900000));
+        
+        isUser.verifyOtp = otp;
+        user.verifyOtpExpireAt = Date.now() + 5 * 60 * 60 * 1000
+        
+        await isUser.save();
+
+        const mailOption = {
+            from: process.env.SENDER_EMAIL,
+            to: isUser.email,
+            subject: 'Notespring account verification OTP',
+            text: `Your OTP is ${otp}. Use this OTP to verify your Notespring account.`
+        }
+
+        await transporter.sendMail(mailOption);
+
+        return res.json({
+          success: false,
+          message: "OTP send successfully"  
+        })
+
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: "Internal server error"
         })
     }
 }
