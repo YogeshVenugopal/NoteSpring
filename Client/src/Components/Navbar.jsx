fe0525f0c90fac8b4bb8b30665f356cdd4ed3f0d
YogeshVenugopal <yogeshvenugopal875@gmail.com>
2026-07-24 15:39:47 +0530

    make some adjuction in the navbar and title logo

--- Client/src/Components/Navbar.jsx
+++ Client/src/Components/Navbar.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Button from './Button'
+import { ListSortDescending } from 'lucide-react'
+import { motion } from 'framer-motion'
 
 function Navbar() {
+    const[isOpen, setIsOpen] = useState(false);
+
+    const handleMenuBar = () => {
+        setIsOpen(!isOpen);
+    }
   return (
     <div className='w-full h-[90px]'>
-        <div className='w-[90%] md:w-[85%] mx-auto flex items-center justify-between py-4'>
+        <div className='w-[90%] md:w-[85%] mx-auto flex items-center justify-between py-4 relative'>
             <div>
                 <p className='font-bold text-[28px] md:text-[32px] dark:text-white'>NoteSpring</p>
             </div>
@@ -13,6 +20,21 @@ function Navbar() {
                 <p className='font-semibold dark:text-white cursor-pointer'>Login</p>
                 <Button className="font-semibold">Sign up</Button>
             </div>
+            <button className='md:hidden cursor-pointer' onClick={handleMenuBar}>
+                <ListSortDescending size={20}/>
+            </button>
+            {
+                isOpen && (
+                    <motion.div 
+                    initial={{height:0}}
+                    animate={{height:"auto"}}
+                    transition={{duration:0.4, ease:"easeInOut"}}
+                    className='md:hidden w-[150px] rounded-2xl overflow-hidden shadow-sm absolute top-full right-0 flex flex-col gap-4 items-center p-4'>
+                        <p>login</p>    
+                        <Button className="w-full">Sign up</Button>                    
+                    </motion.div>
+                )
+            }
         </div>
     </div>
   )
